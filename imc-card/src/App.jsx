8a8bd384d86a8a78be6fc8a626f3bdf2ad52de98
silveirabrenda <brendatuche@proton.me>
2026-01-12 16:43:25 -0300

    limpando branch nova

--- imc-card/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import CardImc from './components/CardImc';
-import './styles/CardImc.css';
-
-function App() {
-  return <CardImc />;
-}
-
-export default App;
--- todolist-useref/src/components/StepItem.jsx
+++
@@ -1,9 +0,0 @@
-const StepItem = ({ step, taskId, onToggle, onDelete }) => (
-  <div className={`step-item ${step.completed ? 'done' : ''}`}>
-    <input type="checkbox" checked={step.completed} onChange={() => onToggle(taskId, step.id)} />
-    <span>{step.text}</span>
-    <button onClick={() => onDelete(taskId, step.id)}>x</button>
-  </div>
-);
-
-export default StepItem;
